7cfd5f486906dfdadbd87f6a5cf21dfd523ddcb6
yuvraj singh <yuvrajjo7.o8@gmail.com>
2026-03-31 21:40:38 +0530

    Create script3.sh

---
+++ script3.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Script 3: Disk and Permission Auditor
+
+DIRS=("/etc" "/var/log" "/home" "/usr/bin" "/tmp")
+
+echo "Directory Audit Report"
+echo "----------------------"
+
+for DIR in "${DIRS[@]}"; do
+    if [ -d "$DIR" ]; then
+        PERMS=$(ls -ld $DIR | awk '{print $1, $3, $4}')
+        SIZE=$(du -sh $DIR 2>/dev/null | cut -f1)
+        echo "$DIR => Permissions: $PERMS | Size: $SIZE"
+    else
+        echo "$DIR does not exist"
+    fi
+done
